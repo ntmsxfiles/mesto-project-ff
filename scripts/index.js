2b142b082c398ba9eef53c6784331d4410faa358
Daniil Grib <gridanil2002@gmail.com>
2024-12-10 21:08:25 +0400

    second

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,18 @@
 // @todo: Темплейт карточки
-const cardList = document.querySelector(".places__list");
+const cardsContainer = document.querySelector(".places__list");
 const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
-function createCard(item) {
+function createCard(cardData) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   const cardImage = cardElement.querySelector(".card__image");
   const cardTitle = cardElement.querySelector(".card__title");
   const deleteButton = cardElement.querySelector(".card__delete-button");
 
-  cardImage.src = item.link;
-  cardImage.alt = "фотография " + item.name;
-  cardTitle.textContent = item.name;
+  cardImage.src = cardData.link;
+  cardImage.alt = "фотография " + cardData.name;
+  cardTitle.textContent = cardData.name;
   
   deleteButton.addEventListener("click", function () {
     deleteCard(cardElement);
@@ -27,7 +27,7 @@ function deleteCard(cardElement) {
 }
 
 // @todo: Вывести карточки на страницу
-initialCards.forEach(function (item) {
-  const cardElement = createCard(item);
-  cardList.append(cardElement);
+initialCards.forEach(function (cardData) {
+  const cardElement = createCard(cardData);
+  cardsContainer.append(cardElement);
 });
